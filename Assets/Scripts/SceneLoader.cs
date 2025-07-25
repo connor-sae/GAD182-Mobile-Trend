24a29b5b6e8e5322493bf193698c22a4126392d5
Connor_Main <connordvdwest@gmail.com>
2025-07-25 11:39:45 +0400

    Added Scene Transitions
     - Transition Manager (non-destroyed singleton)
     - Transition Animation

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public GameObject TransitionCanvas;
+    public float transitionLength = 4;
+    public float transitionTime = 2;
+    private string requestedScene;
+
+    public static SceneLoader Instance;
+
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+   
+    public void Load(string sceneName)
+    {
+        requestedScene = sceneName;
+        StartCoroutine(Transition());
+    }
+
+    private IEnumerator Transition()
+    {
+        TransitionCanvas.SetActive(true);
+        yield return new WaitForSeconds(transitionTime);
+        SceneManager.LoadScene(requestedScene);
+        yield return new WaitForSeconds(transitionLength - transitionTime);
+        TransitionCanvas.SetActive(false);
+    }
+
+}
